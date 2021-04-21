4852c7362f2e931981770b8d6723e98dbc5def0d
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-04-20 22:54:52 -0300

    Finishing the Welcome page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+
 import { Welcome } from './src/pages/Welcome';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
 
 export default function App(){
+  const [ fontsLoader ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoader)
+    return <AppLoading />
+    
   return(
        <Welcome />
   );
