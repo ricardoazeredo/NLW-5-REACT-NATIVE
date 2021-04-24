0f6688cf52e32997359c5166f555443cc0242be4
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-04-23 21:25:38 -0300

    Adding AsyncStore in app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -12,11 +13,22 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Header(){
+  const [userName, setUserName] = useState<string>();
+
+  useEffect(() => {
+    async function loadStorageUserName(){
+      const user = await AsyncStorage.getItem('@plantmanager:user');
+      setUserName(user || ''); 
+    }
+    loadStorageUserName();
+
+  },[]);
+
   return(
     <View style={styles.container}>
       <View>
         <Text style={styles.greeting}>Olá,</Text>
-        <Text style={styles.userName}>Ricardo</Text>
+        <Text style={styles.userName}>{userName}</Text>
       </View>
       
       <Image source={userImg} style={styles.image} />
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -44,8 +44,7 @@ export function PlantSelect() {
 
   const [page, setPage] = useState(1);
   const [loadingMore, setLoadingMore] = useState(true);
-  const [loadedAll, setLoadedAll] = useState(false)
-  
+    
   async function fetchPlants(){
     const { data } = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
 
@@ -126,6 +125,7 @@ export function PlantSelect() {
       <View>
         <FlatList 
           data={enviroments}
+          keyExtractor={(item) => String(item.key)}
           renderItem={({ item }) => (
             <EnviromentButton 
               title={item.title} 
@@ -142,6 +142,7 @@ export function PlantSelect() {
       <View style={styles.plants}>
             <FlatList
               data={filteredPlants}
+              keyExtractor={(item) => String(item.id)}
               renderItem={({ item }) => (
                 <PlantCardPrimary data={item} />
               )
