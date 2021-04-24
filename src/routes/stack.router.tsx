c9bf9877dd76f14faa2c882c5f7c56d769c30497
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-04-24 02:13:56 -0300

    creating a mix navigation

--- src/routes/stack.router.tsx
+++ src/routes/stack.router.tsx
@@ -4,11 +4,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
+
 import { PlantSave } from '../pages/PlantSave';
 import { MyPlants } from '../pages/MyPlants';
 
 import colors from '../styles/colors';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -36,7 +37,7 @@ const AppRoutes: React.FC = () => (
 
     <stackRoutes.Screen 
       name="PlantSelect"
-      component={PlantSelect}
+      component={AuthRoutes}
     />
 
     <stackRoutes.Screen 
@@ -46,7 +47,7 @@ const AppRoutes: React.FC = () => (
     
     <stackRoutes.Screen 
       name="MyPlants"
-      component={MyPlants}
+      component={AuthRoutes}
     />
 
   </stackRoutes.Navigator>
