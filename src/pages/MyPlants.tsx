cea70a78d2fd032185701192248eb83c7fa3ceab
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-04-24 13:14:14 -0300

    Adding Schedule Notification received

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -11,7 +11,7 @@ import { Header } from '../components/Header';
 import waterdrop from '../assets/waterdrop.png';
 import colors from '../styles/colors';
 import { FlatList } from 'react-native-gesture-handler';
-import { loadPlant, PlantProps, StoragePlantProps } from '../libs/storage';
+import { loadPlant, PlantProps, removePlant } from '../libs/storage';
 import { formatDistance } from 'date-fns';
 import { pt } from 'date-fns/locale';
 import fonts from '../styles/fonts';
@@ -35,15 +35,7 @@ export function MyPlants() {
         text: 'Sim 😪',
         onPress: async () => {
           try {
-            const data = await AsyncStorage.getItem('@plantmanager:plants');
-            const plants = data ? (JSON.parse(data) as StoragePlantProps):{};
-
-            delete plants[plant.id];
-
-            await AsyncStorage.setItem(
-              '@plantmanager:plants',
-              JSON.stringify(plants)
-            );
+            await removePlant(plant.id);
 
             setMyPlants((oldData) => 
               oldData.filter((item) => item.id !== plant.id)
